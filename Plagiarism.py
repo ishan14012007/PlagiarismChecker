b23ae36d3fead101a496ec05f27febc005b5217e
Ishan Bharti <ishanbharti1@gmail.com>
2025-11-24 16:15:46 +0530

    Update Plagiarism.py

--- Plagiarism.py
+++ Plagiarism.py
@@ -1,36 +1,64 @@
 from difflib import SequenceMatcher 
 
+d1 = ""
+d2 = ""
+similarity = 0
+fb = ""
+
 def check(name):
-    with open(name, 'r') as file:
-        return file.read()
+    global d1, d2
+    file = open(name, 'r')
+    content = file.read()
+    file.close()
+    if d1 == "":
+        d1 = content
+    else:
+        d2 = content
 
 def checker(t1, t2):
-    match = round(SequenceMatcher(None, t1, t2).ratio() * 100, 2)
-    return match
+    global similarity
+    obj = SequenceMatcher(None, t1, t2)
+    rv = obj.ratio()
+    pv = rv * 100
+    match = round(pv , 2)
+    similarity = match
 
 def remarks1(percent):
+    global fb
     if percent < 10:
-        return "completely different"
+        fb = "completely different"
     elif percent < 20:
-        return "barely similar, mostly original"
+        fb = "somewhat similar majority portion is original"
     elif percent < 40:
-        return "a few matching parts"
+        fb = "a little bit of similarity"
     elif percent < 60:
-        return "noticeable similarities"
+        fb = "very much same"
     elif percent < 80:
-        return "a lot is same"
+        fb = "too much identical very high copied content"
     else:
-        return "almost identical"
+        fb = "almost totally same"
 
-print("\nSimple Plagiarism Checker\n")
+heading = "\nSimple Plagiarism Checker\n"
+print(heading)
 
-first = input("Enter first file name: ")
-second = input("Enter second file name: ")
+input1 = "Give the first file which needs to be checked "
+first = input(input1)
 
-d1 = check(first)
-d2 = check(second)
+input2 = "Give the other file for checking "
+second = input(input2)
 
-similarity = checker(d1, d2)  
-remarks = remarks1(similarity)  
-print(f"\nSimilarity: {similarity}%")
-print(f"Feedback: {remarks}\n")
+check(first)
+check(second)
+
+checker(d1, d2)
+similarity_score = similarity
+
+score = similarity_score
+remarks1(score)
+final = fb
+
+output_line1 = f"\nSimilarity: {similarity}%"
+print(output_line1)
+
+output_line2 = f"Feedback: {fb}\n"
+print(output_line2)
